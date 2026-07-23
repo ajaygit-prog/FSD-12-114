4b1800384fafbdcb83beef493c876cc1f77dfcd0
ajaygit-prog <ajayprakashmishra1001@gmail.com>
2026-07-23 11:53:42 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,13 @@
+const sum = (a,b) =>{
+    console.log(a+b) ;
+} ;
+
+function sum1(a,b) {
+    console.log(a+b) ;
+}
+
+const sum2 = (a,b)=> console.log(a + b ) ;
+
+sum(2,3) ;
+sum1(3,5) ;
+sum2(7,6) ; 
