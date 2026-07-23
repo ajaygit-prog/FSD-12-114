2a2bd9ff0de4d503dc89762e3177b30487b41dce
ajaygit-prog <ajayprakashmishra1001@gmail.com>
2026-07-23 12:15:24 +0530

    initial commit

---
+++ lab1/fun.js
@@ -0,0 +1,18 @@
+//waf to take any digit (0-9) and 
+//return its in word 
+//0- one
+//5 - five ;
+function print(a) {
+    if (a == 0) console.log("zero");
+    else if (a == 1) console.log("one");
+    else if (a == 2) console.log("two");
+    else if (a == 3) console.log("three");
+    else if (a == 4) console.log("four");
+    else if (a == 5) console.log("five");
+    else if (a == 6) console.log("six");
+    else if (a == 7) console.log("seven");
+    else if (a == 8) console.log("eight");
+    else if (a == 9) console.log("nine");
+
+}
+print(5) ;
